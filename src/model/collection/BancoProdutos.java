fc4db4843e719e10f70de103fe9290423cb1ca68
Davi Oliveira <davisouzaoliveira1975@gmail.com>
2025-03-09 01:26:35 -0300

    BancoProdutos
    Finalizado

--- src/model/collection/BancoProdutos.java
+++ src/model/collection/BancoProdutos.java
@@ -1,12 +1,13 @@
-package model.collection;
+package model.colletction;
 
 import java.util.ArrayList;
+
 import model.entity.Produto;
 import model.entity.Administrador;
-import model.entity.AdministradorCargo;
 
 public class BancoProdutos {
 	private int quantidade;
+	private Administrador adm = new Administrador();
 	private long idAtual = 1;
 	// array para armazenar os produtos
 	private ArrayList<Produto> produtos;
@@ -48,55 +49,84 @@ public class BancoProdutos {
 	}
 	
 	
-	public boolean adicionarProduto(Produto produto, Object user) {
-		
+	public boolean adicionarProduto(Produto produto) {
 		// verifica se é um adm
-		
-			if(verificarPermissao(user)) {
-				produto.setId(idAtual++);
-				produtos.add(produto);
-				quantidade++;
-				return true;
-			}
+		if(adm!=null) {
+			produto.setId(idAtual++);
+			produtos.add(produto);
+			quantidade++;
+			return true;
+		} else {
 			return false;
 		}
-	
+	}
 	
 	public boolean removerProdutos(Produto produto, Object user) {
 		// verifica se é um adm
 		if(verificarPermissao(user)) {
+			
 			// verifica se o produto está no array
 			if(produtos.contains(produto)) {
 				produtos.remove(produto);
 				quantidade--;
 				return true;
-			}else {
-				return false;
+			} else {
+				return false
 			}
+			
+		} else {
+			return false;
+			
 		}
-		return false;
 	}
 	
 	public boolean editarProdutos(long buscarId,Produto produtoAtual, Object user) {
-		
 		if(verificarPermissao(user)) {
 			for(Produto produto:produtos) {
 				if(produto.getId() == buscarId) {
 				produto.setNome(produtoAtual.getNome());
 				produto.setDescricao(produtoAtual.getDescricao());
 				produto.setQuantidade(produtoAtual.getQuantidade());
-				produto.setValorProduto(produtoAtual.getValorProduto());
+				produto.setPreco(produtoAtual.getPreco());
+				System.out.println("Produto editdo.");
 				// finalizar o looping
 				return true;
 				}
-			
+				
+				return false;
 			}
 		}
+		
+		return false;
+	}
+	
+	public void listar() {
+		System.out.println(this.toString());
+	}
+	
+	public boolean vendaProduto(long idProduto, double valorProduto, long idUsuario) {
+		BancoUsuarioPadrao bancoUsuario = new BancoUsuarioPadrao();
+		
+		for(Produto produto:produtos) {
+			if(produto.getId() == idProduto && produto.getQuantidade() > 0) {
+				produto.setQuantidade(produto.getQuantidade() - 1);
+				
+				if(produto.getQuantidade == 0) {
+					System.out.println("O estoque do: " + produto.getNome() + " está 0.");
+				}
+				
+				if(bancoUsuario.comissao(idUsuario, valorProduto) && bancoUsuario.vendaQuantidade(idUsuario)) {
+						return true;
+				}
+			}
+			
+			return false;
+		}
+		
 		return false;
 	}
 	
 	public boolean verificarPermissao(Object user) {
-		
 		if(user instanceof Administrador) {
 			Administrador adm = (Administrador) user;
 			
@@ -104,11 +134,9 @@ public class BancoProdutos {
 				return true;
 			}
 		}
+		
 		return false;
 	}
 	
-	public void listar() {
-		System.out.println(this.toString());
-	}
 	
 }
