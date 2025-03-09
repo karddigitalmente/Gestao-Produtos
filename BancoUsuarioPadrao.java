3f1f071b3e50d475304a97b8c03ac618afb05e59
PedrAugusto <100167472+Pedraugust0@users.noreply.github.com>
2025-03-09 14:29:52 -0300

    Resolvidos bugs e adicionado método de remover
    Correção de bugs para adicionar ao DB e adição método de remover e incrementações no método adicionar

---
+++ BancoUsuarioPadrao.java
@@ -0,0 +1,135 @@
+package model.collection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dao.UsuarioDAO;
+import model.entity.Administrador;
+import model.entity.AdministradorCargo;
+import model.entity.Usuario;
+import util.ConexaoBanco;
+
+public class BancoUsuarioPadrao {
+	
+	private int quantidade;
+	
+	// Id gerenciado pelo programa
+	private long idAtual = 1;
+	private ArrayList<Usuario> usuarios;
+	
+	public BancoUsuarioPadrao() {
+		quantidade = 0;
+		usuarios = carregar();
+		
+	}
+	
+	public void adicionar(Usuario usuario, Object user) {
+		// Id gerenciado pelo programa
+		if(user instanceof Administrador) {
+			Administrador adm = (Administrador) user;
+			if(adm.getCargo() == AdministradorCargo.GERENTE) {
+				usuario.setId(idAtual++);
+				usuarios.add(usuario);
+				UsuarioDAO.salvar(usuario);
+				
+				quantidade++;
+			}
+		}
+		
+	}
+	
+	public static void remover(long id) {
+		String query = "DELETE FROM Usuario WHERE id=?";
+		
+		// Executar a query
+		try(Connection conn = ConexaoBanco.getConnection();
+			PreparedStatement statement = conn.prepareStatement(query)) {
+			
+			statement.setLong(1, id);
+			
+			int linhasAfetadas = statement.executeUpdate();
+			
+			if(linhasAfetadas == 0) {
+				System.out.println("Não foi encontrado usuário com este id!!");
+			} else {
+				System.out.println("Usuário Removido com Sucesso!");
+			}
+			
+		} catch (SQLException e) {
+			System.err.println("Erro ao salvar o usuário: " + e.getMessage());
+		}
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	public void listar() {
+		System.out.println(toString());
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder resultado = new StringBuilder();
+		
+		// Para não printar o newLine ao chegar no último produto
+		int count = 1;
+		
+		for(Usuario usuario : usuarios) {
+			resultado.append(usuario.getId() + " " + usuario.getNome() + " " + usuario.getCpf() + " " + usuario.getSenha() + " " + usuario.getTurno() );
+			
+			// Comentário acima kk
+			if(count < quantidade) {
+				resultado.append("\n");
+			}
+						
+			count++;
+		}
+		
+		return resultado.toString();	
+	}
+	
+	
+	
+	public boolean comissao(long id, double valorProduto) {
+	
+		for(Usuario usuario : usuarios) {
+			
+			if(usuario.getId() == id) {
+				usuario.setComissao(usuario.getComissao() + valorProduto * usuario.getTaxaVenda());
+				vendaQuantidade(id);
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	
+	//Decidir se vai juntar com produto ou deixar separado.
+	public boolean vendaQuantidade(long id) {
+		
+		for(Usuario usuario : usuarios) {
+			if(usuario.getId() == id) {
+				usuario.setVendasTotais(usuario.getVendasTotais() +1);
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	
+	public static ArrayList<Usuario> carregar() {
+		return UsuarioDAO.carregar();
+	}
+	
+}
---
+++ UsuarioDAO.java
@@ -0,0 +1,123 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.entity.Turno;
+import model.entity.Usuario;
+import util.ConexaoBanco;
+
+public class UsuarioDAO {
+
+	public static void salvar(Usuario user) {
+		String query = "INSERT INTO Usuario(id, nome, cpf, senha, turno, salario, comissao, taxa) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		// Executar a query
+		try(Connection conn = ConexaoBanco.getConnection();
+			PreparedStatement statement = conn.prepareStatement(query)) {
+			
+			statement.setLong(1, user.getId());
+			statement.setString(2, user.getNome());
+			statement.setString(3, user.getCpf());
+			statement.setString(4, user.getSenha());
+			statement.setString(5, user.getTurno().toString());
+			statement.setDouble(6, user.getSalario());
+			statement.setDouble(7, user.getComissao());
+			statement.setFloat(8, user.getTaxaVenda());
+		
+			
+			int linhas = statement.executeUpdate();
+			
+			if(linhas > 0) {
+				System.out.println("Usuário adicionado com sucesso");
+			} else {
+				System.out.println("Usuário não adicionado");
+			}
+			
+		} catch (SQLException e) {
+			System.err.println("Erro ao salvar o usuário: " + e.getMessage());
+		}
+		
+	}
+	
+	public static void pegar(long id) {
+		String query = "SELECT * FROM U;suario WHERE id=?";
+		Usuario usuario;
+		String nome, cpf, senha;
+		Turno turno;
+		double comissao;
+		double salario;
+		float taxa;
+		
+		// Executar a query
+		try(Connection conn = ConexaoBanco.getConnection();
+			PreparedStatement statement = conn.prepareStatement(query)) {
+			
+			statement.setString(1, Long.toString(id));
+			
+			ResultSet result = statement.executeQuery();
+			
+			if(result.next()) {
+				nome = result.getString("nome");
+				cpf = result.getString("cpf");
+				senha = result.getString("senha");
+				turno = Turno.valueOf(result.getString("turno"));
+				comissao = result.getDouble("comissao");
+				salario = result.getDouble("salario");
+				taxa = result.getFloat("taxa");
+				
+				usuario = new Usuario(id, nome, cpf, senha, turno, comissao, salario, taxa);
+					
+				System.out.println(usuario);
+			} else {
+				System.out.println("Usuário Não encontrado!");
+			}
+			
+		} catch (SQLException e) {
+			System.err.println("Erro ao recuperar o usuário: " + e.getMessage());
+		}
+		
+	}
+	
+	public static ArrayList<Usuario> carregar() {
+		ArrayList<Usuario> usuarios = new ArrayList<>();
+		String query = "SELECT * FROM Usuario";
+		
+	try(Connection conn = ConexaoBanco.getConnection();
+		PreparedStatement statement = conn.prepareStatement(query)){
+		Usuario usuario;
+		String nome, cpf, senha;
+		Turno turno;
+		double comissao;
+		double salario;
+		float taxa;
+		long id;
+		
+		ResultSet result= statement.executeQuery();
+		
+		while(result.next()) {
+			id = result.getLong("id");
+			nome = result.getString("nome");
+			cpf = result.getString("cpf");
+			senha = result.getString("senha");
+			turno = Turno.valueOf(result.getString("turno"));
+			comissao = result.getDouble("comissao");
+			salario = result.getDouble("salario");
+			taxa = result.getFloat("taxa");
+			
+			usuario = new Usuario(id, nome, cpf, senha, turno, comissao, salario, taxa);
+			usuarios.add(usuario);
+		}
+		
+		return usuarios;
+		
+		
+	}catch(SQLException e) {
+		throw new RuntimeException("Erro ao carregar usuários" + e.getMessage());
+		}
+	}
+	
+}
