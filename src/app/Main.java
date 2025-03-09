3525858b248bfda07d5f20e95c0ac6f4aab6c520
Davi Oliveira <davisouzaoliveira1975@gmail.com>
2025-03-09 15:11:39 -0300

    Main - metodo criarAdm

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,56 +3,68 @@ package app;
 import java.util.Scanner;
 
 import model.collection.BancoAdministrador;
+import model.entity.Administrador;
 import model.entity.AdministradorCargo;
+
 public class Main {
 	static Scanner sc = new Scanner(System.in);
-	static BancoAdministrador adm;
+	static BancoAdministrador bancoAdm;
+	static Administrador adm;
+	//conta padrao
+	static Administrador padrao = new Administrador("padrao","00000000000","system", AdministradorCargo.GERENTE);
 	
 	public static void main(String[] args) {
-		adm = new BancoAdministrador();
+		bancoAdm = new BancoAdministrador();
+
 		
 		System.out.println("!!Bem vindo!!");
-		System.out.print("1-Login\n>");
-		
+		System.out.print("1- Login\n> ");
 		int escolha = sc.nextInt();
 		
 		switch(escolha) {
-		
 		case 1:
-			long id;
-			String nomeAdm;
-			String cpf;
-			String senha;
-			int cargo;
-			AdministradorCargo obj;
-			if(adm.getAdminsitradores().size() == 0) {
-				System.out.print("Vamos criar uma conta administrador?\n" 
-								+ "Informe o id: ");
-				id = sc.nextLong();
-				sc.nextLine();
-				System.out.print("Informe o seu nome: ");
-				nomeAdm = sc.nextLine();
-				System.out.print("Informe o seu cpf: ");
-				cpf = sc.nextLine();
-				System.out.print("Informe a sua senha: ");
-				senha = sc.nextLine();
-				System.out.print("Informe o seu cargo: \n" 
-								+ "1-GERENTE"
-								+ "\n2-SUPERVISOR_PRODUTOS"
-								+ "\n3-SUPERVISOR_REVENDEDORES");
-				cargo = sc.nextInt();
+			criarAdm();
 				
-				sc.nextLine();
-				if(cargo != 1) {
-					System.out.print("Você não possui permissão!");
-				}else {
-					obj = AdministradorCargo.GERENTE;
-				}
-				
-			}
+			
+		
+		sc.close();
 		}
-		
-		
 	}
 	
+	public static void criarAdm() {
+		String nomeAdm, cpf, senha;
+		int cargo;
+		
+		AdministradorCargo obj;
+
+		// verifica se existe alguma conta de adm
+		if(bancoAdm.getAdminsitradores().size() == 0) {
+			System.out.print("Você não possui uma conta administrador, vamos criar!!\n" 
+			+ "Informe seu nome: ");
+			nomeAdm = sc.next();
+			System.out.print("Informe seu cpf: ");
+			cpf = sc.next();
+			System.out.print("Informe sua senha: ");
+			senha = sc.next();
+			System.out.print("Informe o seu cargo: \n" 
+							+ "1- Gerente"
+							+ "\n2- Supervisor de Produtos"
+							+ "\n3- Supervisor de Revendedores\n> ");
+			cargo = sc.nextInt();
+			if(cargo != 1) {
+				System.out.print("Você não possui permissão!");
+				return;
+			} 
+			
+			obj = AdministradorCargo.GERENTE;
+			adm = new Administrador(nomeAdm, cpf, senha, obj);
+			if(bancoAdm.adicionar(adm, padrao)) {
+				System.out.print("Conta criada com sucesso!");
+			} else {
+				System.out.print("Erro ao criar a conta.");
+			}
+			
+		}
+	}
 }
+		
